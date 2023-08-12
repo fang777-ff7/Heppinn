5c8f3ffcb6b893d9134037ed50a90589d03a27fb
fang777-ff7 <139101672+fang777-ff7@users.noreply.github.com>
2023-08-12 13:22:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -32,8 +32,41 @@ def callback():
 #處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+
+    emoji = [
+            {
+                "index": 0,
+                "productId": "11537",
+                "emojiId": "52002738"
+            },
+            {
+                "index": 10,
+                "productId": "11537",
+                "emojiId": "52002738"
+            }
+    ]
+
+    text_message = TextSendMessage(text='''$ Heppinn $
+Hello! 您好 歡迎您成為 Heppinn 的好友!
+
+我是超級顧問 很高興為您服務
+                                   
+-這裡有股票、匯率資訊喔~
+-直接點選下方【圖中】選單功能
+                                   
+-期待您的光臨!''', emojis=emoji)
+    
+    sticker_message = StickerSendMessage(
+        package_id='11537',
+        sticker_id='52002738'
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message, sticker_message])
+
+
+    # message = TextSendMessage(text=event.message.text)
+    # line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
